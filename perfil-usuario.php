9b31b32c222bffb90d8d4146eed9d6f7eb7cbf86
Alexhumme <93412660+Alexhumme@users.noreply.github.com>
2023-10-22 19:43:06 -0500

    pe

--- perfil-usuario.php
+++ perfil-usuario.php
@@ -1,33 +1,55 @@
-<?php include('./php/auth.php')?>
+<?php include('./php/auth.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<?php include_once('./componentes/head.php');?>
+<?php include_once('./componentes/head.php'); ?>
+
 <body>
-<?php include_once('./componentes/user-header.php');?>
+    <?php include_once('./componentes/user-header.php'); ?>
     <main>
         <h3 class="vista-titulo">Perfil de usuario</h3>
         <div class="container">
             <form action='php/actualizar_informacion_usuario.php' method='POST'>
                 <div class="ordenar-inf">
                     <div>
-                     <input type="text" class="form-control" value="<?php echo $_SESSION['user']['nombre'] ?>" name="nombre">
+                        <input type="text" class="form-control" disabled value="<?php echo $_SESSION['user']['nombre'] ?>" name="nombre">
                     </div>
                     <div>
-                     <input type="text" class="form-control" value="<?php echo $_SESSION['user']['apellidos'] ?>"  name="apellidos">
+                        <input type="text" class="form-control" disabled value="<?php echo $_SESSION['user']['apellidos'] ?>" name="apellidos">
                     </div>
                 </div>
                 <div class="ordenar-inf">
                     <div>
-                     <input type="email" class="form-control" value="<?php echo $_SESSION['user']['email'] ?>" name="email">
+                        <input type="email" class="form-control" disabled value="<?php echo $_SESSION['user']['email'] ?>" name="email">
                     </div>
                     <div>
-                     <input type="date" class="form-control" value="<?php echo $_SESSION['user']['fecha'] ?>" name="fecha">
+                        <input type="date" class="form-control" disabled value="<?php echo $_SESSION['user']['fecha'] ?>" name="fecha">
                     </div>
                 </div>
-                <button class="boton_usuario">Registrar</button>
+                <div class="ordenar-inf">
+                    <div>
+                        <button class="boton_usuario activate" type="button" onclick="switchEditMode()">Editar</button>
+                    </div>
+                    <div>
+                        <button class="boton_usuario" type="submit" name="save" value="save">Guardar</button>
+                    </div>
+                    <div>
+                        <button class="boton_usuario cancel" type="button" onclick="switchEditMode()">Cancelar</button>
+                    </div>
+                </div>
             </form>
         </div>
     </main>
+    <script>
+        let editMode = false;
+        const switchEditMode = () => {
+            editMode = !editMode;
+            document.querySelectorAll(".form-control").forEach((input) => input.disabled = !editMode);
+            document.querySelectorAll("button").forEach((button) => {
+
+            })
+        };
+    </script>
 </body>
+
 </html>
--- php/actualizar_informacion_usuario.php
+++ php/actualizar_informacion_usuario.php
@@ -9,23 +9,34 @@
     $update_sql = "UPDATE registro SET nombre='$nombre', apellidos='$apellidos', email='$email', fecha='$fecha' WHERE nombre='$nombre'";
     $update_query = mysqli_query($conexion, $update_sql);
 
-    if ($update_query) {
-        echo '
-        <script>
-        alert("Informacion Actualizada");
-        window.location="../perfil-usuario.php";
-        </script>
-        '; // Envía una respuesta al cliente para indicar que se guardó con éxito
-    } else {
-        echo '
-        <script>
-        alert("Informacion no Actualizada");
-        window.location="../perfil-usuario.php";
-        </script>
-        ';
-    }
+    // if ($update_query) {
+    //     echo '
+    //     <script>
+    //     alert("Informacion Actualizada");
+    //     window.location="../perfil-usuario.php";
+    //     </script>
+    //     '; // Envía una respuesta al cliente para indicar que se guardó con éxito
+    // } else {
+    //     echo '
+    //     <script>
+    //     alert("Informacion no Actualizada");
+    //     window.location="../perfil-usuario.php";
+    //     </script>
+    //     ';
+    // }
 
 
 
  mysqli_close($conexion);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $_POST['action'];?>
+</body>
+</html>
