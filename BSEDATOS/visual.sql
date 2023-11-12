0a6049e78744eec161b1b5596b6941ca90d0b5a3
Alexhumme <93412660+Alexhumme@users.noreply.github.com>
2023-11-12 02:06:17 -0500

    base de datos

--- BSEDATOS/visual.sql
+++ BSEDATOS/visual.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-10-2023 a las 04:23:52
+-- Tiempo de generación: 12-11-2023 a las 08:02:30
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.0.13
 
@@ -53,9 +53,9 @@ CREATE TABLE `prestamo` (
 --
 
 INSERT INTO `prestamo` (`id_prestamo`, `id_usuario`, `direccion`, `telefono`, `dia_solicitado`, `hora`, `cantida_prestamo`, `estado`) VALUES
-(3, 1118808995, '1111111', 11111111, '0001-11-11', '11:11:00', '9112', 'Aceptada'),
-(4, 1118808995, '22222', 22222, '0022-02-22', '22:22:00', '212346', 'Rechazada'),
-(5, 1118805275, '333333', 3333333, '0000-00-00', '03:33:00', '33333', 'Rechazada');
+(3, 1118808995, '1111111', 11111111, '0001-11-11', '11:11:00', '9112', 'Rechazada'),
+(4, 1118808995, '22222', 22222, '0022-02-22', '22:22:00', '212346', 'Pendiente'),
+(5, 1118805275, '333333', 3333333, '0000-00-00', '03:33:00', '33333', 'Pendiente');
 
 -- --------------------------------------------------------
 
@@ -66,6 +66,7 @@ INSERT INTO `prestamo` (`id_prestamo`, `id_usuario`, `direccion`, `telefono`, `d
 CREATE TABLE `registro` (
   `nombre` varchar(60) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `apellidos` varchar(60) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `direccion` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL DEFAULT '',
   `ident` int(12) NOT NULL,
   `email` varchar(60) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `estado` varchar(60) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
@@ -77,14 +78,14 @@ CREATE TABLE `registro` (
 -- Volcado de datos para la tabla `registro`
 --
 
-INSERT INTO `registro` (`nombre`, `apellidos`, `ident`, `email`, `estado`, `fecha`, `admin`) VALUES
-('oscar', 'beto', 44, 'omarpimienta0208@gmail.com', 'casad@', '2023-09-12', 0),
-('carlos', 'martines', 45, 'gf@gmail.com', 'union libre', '2023-09-07', 0),
-('natalia', 'dd', 66, 'gabriel.godin@misena.edu.co', 'casad@', '2023-08-29', 0),
-('gabriel', 'godin', 111, 'gabrielgodin02@gmail.com', 'solter@', '2023-09-03', 1),
-('pedro', 'godin', 333, 'pitchmarkel7@hotmail.com', 'solter@', '2023-09-12', 0),
-('Alex', 'Valdelamar Bustamante', 1118805275, 'avaldelamar57@misena.edu.co', 'solter@', '2004-08-04', 1),
-('Johanna', 'Bustamante', 1118808995, 'Jovis@gmail.com', 'union libre', '1985-08-07', 0);
+INSERT INTO `registro` (`nombre`, `apellidos`, `direccion`, `ident`, `email`, `estado`, `fecha`, `admin`) VALUES
+('oscar', 'beto', '', 44, 'omarpimienta0208@gmail.com', 'casad@', '2023-09-12', 0),
+('carlos', 'martines', '', 45, 'gf@gmail.com', 'union libre', '2023-09-07', 0),
+('natalia', 'dd', '', 66, 'gabriel.godin@misena.edu.co', 'casad@', '2023-08-29', 0),
+('gabriel', 'godin', '', 111, 'gabrielgodin02@gmail.com', 'solter@', '2023-09-03', 1),
+('pedro', 'godin', '', 333, 'pitchmarkel7@hotmail.com', 'solter@', '2023-09-12', 0),
+('Alex', 'Valdelamar Bustamante', 'cr24b#12b', 1118805275, 'avaldelamar57@misena.edu.co', 'casad@', '2004-08-03', 1),
+('Johanna', 'Bustamante', '', 1118808995, 'Jovis@gmail.com', 'union libre', '1985-08-07', 0);
 
 --
 -- Índices para tablas volcadas
